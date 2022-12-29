171531c5e3d3a86a111da4134c950bee991b3c3d
pawelswierczynski <pawelswierczynski@pro.wp.pl>
2022-12-29 10:48:15 +0100

    Add a basic main menu navigation

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace SpriteChaser
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
